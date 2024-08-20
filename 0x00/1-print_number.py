d64d13acb05f48ac1a2ec7490bf465554f21352d
karlabeyancer <sict.research@gmail.com>
2024-08-20 19:29:44 +0100

    Python Home work

---
+++ 0x00/1-print_number.py
@@ -0,0 +1,6 @@
+#Program to display numbers
+
+number1 = 5
+number2 = 25
+# Displaying the numbers
+print(number1+number2)
---
+++ 0x00/1a-enter-print numbers.py
@@ -0,0 +1,13 @@
+#Practising input
+## Taking input from the user
+
+
+
+num1 = int(input("Enter a number and press enter: "))  #without the type (int), 
+num2 = int(input("Enter another number and press enter: ")) #it returns a string
+
+#Store the number in a variable (toTal)
+total = num1 + num2
+
+#Display the answer
+print("The answer is ", total)
