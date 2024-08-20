3f8e937074ae404ffa2bb750d42bc7760ef62f55
karlabeyancer <sict.research@gmail.com>
2024-08-20 14:26:47 +0100

    Python Exercise

---
+++ 0x00/0-hello_world.py
@@ -0,0 +1,7 @@
+# A module that contains info on variables and its usage
+# Author: Karl Azoms
+
+
+firstname = input("Enter your 1st name: ") #"Karl"
+lastname = input("Enter your lastname: ") #"Azoms"
+print(firstname + " " +lastname)
