f4ad7881246b8800e64ed44e2280dedca02e3992
karlabeyancer <sict.research@gmail.com>
2024-08-22 14:17:24 +0100

    Day 2 round up

---
+++ 0x01/2-print_aphabet.py
@@ -0,0 +1,23 @@
+#Printing ASCII Codes
+
+"""
+Write a program that prints the ASCII alphabets in lower case not followed by a new line
+
+Hint (Use end inside the print statement), ord(), chr()
+
+Instruction
+You can only use one print function with string format
+You can only use one loop
+You are not allowed to store character in a variable
+You are not allowed to import any module
+
+"""
+
+#Create an ascii number
+
+
+#asc_val = ord(input("Enter any character: "))
+alphabet_asc = chr(int(input("Enter an integer ")))
+print(alphabet_asc)
+#print(asc_val)
+
---
+++ 0x01/3-last_digit.py
@@ -0,0 +1,36 @@
+#Generating random by importing "random library"
+
+"""
+Question: Write a Program that will assign a random signed number to the variable number
+each time it is executed.
+Your Program should compute the last digit of the random number.The output of the program should be
+The last digit of...followed by the number, followed by ...is... followed by the last digit...followed by
+ If the last digit is greater than 5, 
+Print last digit is greater than 5
+If the last Digit is zero, print last digit and is zero
+If the last digit less than 6 and not zero, print the string "last digit and is less than 6 not zero
+"""
+
+import random
+
+
+#Generating random number between -10000 and 10000
+number = random.randint(-10000, 10000)
+#print(number)
+#lastdigit =number / 10
+lastdigit = abs(number) % 10  #Modulus division  - negative numbers does
+#last = number // 10  #interger division
+#print(f"Last digit of {number} is ", end="" )   Variant of string formatting
+print("the last digit of {} is ".format(number), end="")  
+if lastdigit >5:
+    print(f"{lastdigit} and is greater than 5")
+elif lastdigit == 0:
+    print(f"{lastdigit} and is 0")
+else:
+    print(f"{lastdigit} and is less than 6 and not zero")
+
+
+
+#print(last)
+
+
