fdf41a980ef4efc39e1072f72ec1428ad21cbae7
karlabeyancer <sict.research@gmail.com>
2024-08-26 05:50:22 +0100

    Assignment on ASCII Updated

--- 0x01/2-print_aphabet.py
+++ 0x01/2-print_aphabet.py
@@ -14,13 +14,25 @@ You are not allowed to import any module
 """
 
 #Solution
+ 
+ #1st attempt worked use only the ord() and chr() and a loop
+
+i = 0
+while i < 26: print(f'{chr(ord("a") + i)}', end=''); i += 1
 
 
-#1st attempt worked
+# #2nd attempt worked using only the range() and chr()    
 
-#print(*(chr(i) for i in range(ord('a'), ord('z') + 1)), sep='', end='') Worked
+# for i in range(97,123):
+#     print(chr(i),end='')  #end='' prevents a new line from being printed after each
 
-#2nd attempt,  worked too
+   
+# 3rd attempt worked use only the ord() and chr() and a loop and a
 
-for i in range(26): print(f'{chr(ord("a") + i)}', end='')
+# print(*(chr(i) for i in range(ord('a'), ord('z') + 1)), sep='', end='') 
+
+#4th attempt,  worked too
+
+for i in range(26): print(f'{chr(ord("a") + i)}', end='')  #Worked... string Formatting applied
+
 
